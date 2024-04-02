a9b051631b1a558e64b56708a48671cc0e436a1c
ciber <jason-vauquelin@laposte.net>
2024-04-02 13:49:58 +0200

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (firstName) {
+    alert(`Bonjour ${firstName} , si on a entré ${firstName} dans l'input field!`);
+    setFirstName('');
+
+    }
+    else {
+      alert(`Champ "prénom" obligatoire`);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setFirstName(event.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Bienvenue chez nous !</h1>
+      <p>Connectez-vous</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrez votre prénom"
+          value={firstName}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Accéder à votre espace</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
---
+++ src/Orderpage.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  
+  return (
+    <>
+    <a href="App.jsx">Déconnexion</a>
+      <h1>Bienvenue sur la page de commande</h1>
+      
+    </>
+  );
+}
+
+export default App;
+
+
